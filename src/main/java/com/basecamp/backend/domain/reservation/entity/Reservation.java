3f179f114525208aff9d2971bbbf9f27073dfcbe
perish95 <perish95@naver.com>
2026-07-07 16:40:53 +0900

    feat: ErrorCode 추가  - 예약 관련 에러 "R001" 추가

--- src/main/java/com/basecamp/backend/domain/reservation/entity/Reservation.java
+++ src/main/java/com/basecamp/backend/domain/reservation/entity/Reservation.java
@@ -73,7 +73,7 @@ public class Reservation {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    // --- 비즈니스 메서드 (상태 변경 도메인 로직) ---
+    // --- 비즈니스 메서드 (상태 변경 도메인 로직) --- TODO: 필요할지 안 할지? -> 쓸거 같기는 함. 일단 두기로
 
     public void pend(){
         this.status = ReservationStatus.PENDING;
@@ -92,15 +92,6 @@ public class Reservation {
         this.status = ReservationStatus.CANCELLED;
         this.cancelDate = LocalDateTime.now();
     }
-
-//    /* 예약 상태 코드 enum */
-//    public static enum ReservationStatus {
-//        PENDING,    // 예약 신청 (대기)
-//        RESERVED,   // 예약 확정 (기존 CONFIRMED 에서 변경)
-//        REJECTED,   // 캠핑업체 거절
-//        CANCELLED   // 예약 취소
-//    }
-
 
 }
 
